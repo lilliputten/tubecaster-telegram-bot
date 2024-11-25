2c29817c4e98a32fd5300e012598f042ab00a726
lilliputten <lilliputten@gmail.com>
2024-11-25 06:01:48 +0300

    Issue #5: Added all the basic commands.

---
+++ bot/commands/commandsInfo.py
@@ -0,0 +1,9 @@
+commandsInfo = {
+    'start': 'Gives information about the bot',
+    'help': 'Gives information about all of the available commands',
+    'cast URL': 'Convert YouTube video to audio for listening',
+}
+
+hiddenCommands = [
+    'test',
+]
