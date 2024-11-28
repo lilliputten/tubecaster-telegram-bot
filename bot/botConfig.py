d7eaaa191b1b3d961919d58204184732ea27f4b8
lilliputten <lilliputten@gmail.com>
2024-11-28 23:21:21 +0300

    Deploy test

--- bot/botConfig.py
+++ bot/botConfig.py
@@ -7,7 +7,7 @@ TELEGRAM_TOKEN = appConfig.get('TELEGRAM_TOKEN')  # Telegram token from env or v
 VERCEL_URL = appConfig.get('VERCEL_URL', '')  # Should be provided by vercel environment for production
 
 WEBHOOK_PREFIX = appConfig.get('WEBHOOK_PREFIX', 'https://')
-WEBHOOK_HOST = appConfig.get('WEBHOOK_HOST', '127.0.0.1')  # Should be an ngrok relay link (for the local mode)
+WEBHOOK_HOST = appConfig.get('WEBHOOK_HOST', '127.0.0.1')  # Should be vds remote host name an ngrok relay link (for the local mode)
 WEBHOOK_PATH = appConfig.get('WEBHOOK_PATH', '/webhook')  # Local route, see implementation in `botBlueprint.py`
 
 # Compose correct webhook fully-qualified url...
