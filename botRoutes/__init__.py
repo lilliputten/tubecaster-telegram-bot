a1b8699c985d0dd2b8bcdef9fdb352ecf2b43b83
lilliputten <lilliputten@gmail.com>
2024-12-18 20:51:00 +0300

    Issue #19: Fixed bot routes.

--- botRoutes/__init__.py
+++ botRoutes/__init__.py
@@ -6,13 +6,20 @@ if not IS_TEST:
 
     from .botRoutes import botRoutes
 
-    from . import _allRoutes, _webhookRoute, _testRoute, _startRoute
+    from . import (
+        _webhookRoute,
+        _rootRoute,
+        _testRoute,
+        _startRoute,
+        _stopRoute,
+    )
 
     __all__ = [
         'botRoutes',
         # The rest are only for internal usage only
-        '_allRoutes',
         '_webhookRoute',
         '_testRoute',
         '_startRoute',
+        '_stopRoute',
+        '_rootRoute',
     ]
